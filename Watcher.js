8e52a435fd722f57664bde337b37f437a24689f7
Joshua Freeman <joshua.freeman@epfl.ch>
2022-02-15 13:48:15 +0100

    Telegram bot integration
    - A bit of type checking stuff
    - Update README.md, cmdline args,etc.
    - Bad code (magic strings and bad input testing) but in the spirit of
    this project, I don't care.

--- Watcher.js
+++ Watcher.js
@@ -14,17 +14,17 @@ module.exports = class Watcher extends EventEmitter {
         this.interval = setInterval(async () => {
             try {
                 const courses = await connect_isa.fetch_courses(ISA_ADDR, token, course_reg_url)
-                const wc = watched_courses
-                const now_available_courses = wc.map(id => courses[id])
-                            .filter(d => !!d)
-                            .filter(d => d.isAvailable)
-                            .filter(d => watched_courses.includes(d.id))
+                const now_available_courses = watched_courses.map(id => courses[id])
+                    .filter(d => !!d)
+                    .filter(d => d.isAvailable)
+                    .filter(d => watched_courses.includes(d.id))
 
                 if (now_available_courses.length > 0) {
                     this.emit('available-course', now_available_courses)
+
                     let unwatch = id => {
                         let ind = watched_courses.indexOf(id)
-            
+
                         if (ind > -1)
                             watched_courses.splice(ind, 1)
                     }
